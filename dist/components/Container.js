08970d57011e72437f5637b961a330eb143cd91c
左翔太朗 <s.hidaridesu@gmail.com>
2024-05-18 17:48:32 +0900

    dist以下の更新

--- dist/components/Container.js
+++ dist/components/Container.js
@@ -1,10 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Container = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function Container({ children }) {
+import { jsx } from "react/jsx-runtime";
+export function Container({ children }) {
     return (
     // TODO: 幅固定の仕方が分からん
-    (0, jsx_runtime_1.jsx)("div", { style: { display: "grid", placeContent: "center", padding: "2rem" } }, children));
+    jsx("div", { style: { display: "grid", placeContent: "center", padding: "2rem" } }, children));
 }
-exports.Container = Container;
--- dist/components/Stack.js
+++ dist/components/Stack.js
@@ -1,12 +1,8 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Stack = void 0;
-const jsx_runtime_1 = require("react/jsx-runtime");
-function Stack({ space = 1, children, }) {
-    return ((0, jsx_runtime_1.jsx)("div", { style: {
+import { jsx } from "react/jsx-runtime";
+export function Stack({ space = 1, children, }) {
+    return (jsx("div", { style: {
             display: "flex",
             flexDirection: "column",
             gap: `${space}rem`,
         } }, children));
 }
-exports.Stack = Stack;
